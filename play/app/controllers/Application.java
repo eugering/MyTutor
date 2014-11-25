fd69d2e54ef5f082b6145a31e5d3c3fccc3f67b2
Metehan Kilin <metehan.kilin@htwg-konstanz.de>
2014-11-25 17:57:21 +0100

    play einbindung

---
+++ play/app/controllers/Application.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+public class Application extends Controller {
+
+    public static Result home() {
+        return ok(home.render());
+    }
+	
+	 public static Result login() {
+        return ok(login.render());
+    }
+    
+     public static Result profilAnzeigen() {
+        return ok(profilAnzeigen.render());
+    }
+    
+     public static Result profilBearbeiten() {
+        return ok(profilBearbeiten.render());
+    }
+    
+     public static Result registrierung() {
+        return ok(registrierung.render());
+    }
+    
+     public static Result tutorWerden() {
+        return ok(tutorWerden.render());
+    }
+    
+     public static Result suchen() {
+        return ok(suchen.render());
+    }
+    
+     public static Result ueberUns() {
+        return ok(ueberUns.render());
+    }
+    
+}
--- play/build.sbt
+++ play/build.sbt
@@ -1,4 +1,4 @@
-name := """myProjectName"""
+name := """play"""
 
 version := "1.0-SNAPSHOT"
 
