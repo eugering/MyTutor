043a958e200ddbac1cae2f8398a4295e414e479b
Metehan Kilin <metehan.kilin@htwg-konstanz.de>
2015-01-24 00:09:33 +0100

    face

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
@@ -406,6 +408,36 @@ public class Application extends Controller {
 	}
     
     
+<<<<<<< HEAD
+	// Methode für das Passwort-Hashing
+    public static String getSecurePassword(String passwordToHash)
+    {
+        String generatedPassword = null;
+       try {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add password bytes to digest
+            md.update(passwordToHash.getBytes());
+            //Get the hash's bytes
+            byte[] bytes = md.digest();
+            //This bytes[] has bytes in decimal format;
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+
+=======
     // Methode für das Passwort-Hashing
 public static String getSecurePassword(String passwordToHash){
                 String generatedPassword = null;
@@ -431,6 +463,7 @@ public static String getSecurePassword(String passwordToHash){
                     }
                 return generatedPassword;
             }
+>>>>>>> 404950c82a8aa17a84f43c589349dfa1b30cf701
 	
 
 }
